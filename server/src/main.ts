32641a2d5e3339f9357287ec875f5cf18b91085f
eYugo <eric.yugo28@gmail.com>
2024-08-21 15:59:44 -0300

    add origin url

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,19 +3,21 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/http-exception.filter';
 import { ErrorsInterceptor } from './error/errors.interceptor';
 
-require('../patch.js'); 
+require('../patch.js');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new HttpExceptionFilter());
   // app.useGlobalInterceptors(new ErrorsInterceptor());
   // Enable CORS
+  const url = process.env.ORIGIN_URL;
+
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: url, // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
+
   await app.listen(3001);
 }
 bootstrap();
